c807f680d1a754977ff5f0aca9173399f1d3c67c
Latoshia Williams <lswillms@yahoo.com>
2022-07-15 09:06:21 -0500

    commit tree 83563047e659779067a0ecfe24eb06ceaf3c4eb9

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+function findMatching (driver, name) {
+    let racer = driver.filter(element => element === name || element === name.toLowerCase())
+    return racer 
+}
+ 
+function fuzzyMatch(driver, name) {
+    let racer = driver.filter(element => element.slice(0,name.length) === name)
+    return racer
+}
+
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function matchName(drivers, hometown) {
+    let name = drivers.filter(driver => driver.name === hometown)
+    return name
+}
+
+
